5a3fb29e88710d3c62a9b77b15cfe5d673763c8e
EternalllZM <43221983+EternalllZM@users.noreply.github.com>
2025-01-10 11:17:58 -0600

    Update __init__.py

--- mutesync/__init__.py
+++ mutesync/__init__.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 from redbot.core.bot import Red
 
-from .bansync import MuteSync
+from .mutesync import MuteSync
 
 with Path(__file__).parent.joinpath("info.json").open() as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
